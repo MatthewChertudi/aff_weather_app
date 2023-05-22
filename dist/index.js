28a38d18964d5fca62cc4fa4417a92681f4f364e
Matthew Chertudi <mchertudi@auvik.com>
2023-05-22 16:46:10 -0600

    proof of concept call

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const https_1 = __importDefault(require("https"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+const lat = process.env.LAT;
+const lon = process.env.LON;
+app.get('/', (_req, res) => {
+    https_1.default.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_API_KEY}`, res => {
+        let data = '';
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            console.log(JSON.parse(data));
+        });
+    });
 });
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import httpsModule from 'https';
+
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const lat = process.env.LAT;
+const lon = process.env.LON;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.get('/', (_req: Request, res: Response) => {
+    httpsModule.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_API_KEY}`, res => {
+        let data = '';
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            console.log(JSON.parse(data));
+        });
+    })
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
