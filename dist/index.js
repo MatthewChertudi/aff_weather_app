a5c513077520ef1152feadd48db37b5e19027e1c
Matthew Chertudi <mchertudi@auvik.com>
2023-05-22 18:33:38 -0600

    continue to destructure data, create types, extract 7 day forecast

--- dist/index.js
+++ dist/index.js
@@ -16,10 +16,37 @@ app.get('/', (_req, res) => {
     let lon = process.env.LON;
     let api_key = process.env.OPEN_WEATHER_API_KEY;
     let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}&units=imperial`;
-    let aggregateData = {};
-    (0, node_fetch_1.default)(url, aggregateData)
+    let aggregateData = {
+        current: {
+            today: '',
+            temp: '',
+            feels_like: '',
+            weather_state: '',
+            weather_icon: '',
+            weather_description: '',
+            wind_speed: '',
+            wind_direction: '',
+        },
+        daily: [],
+    };
+    (0, node_fetch_1.default)(url)
         .then(res => res.json())
         .then(weather_data => {
+        let daily = (weather_data) => {
+            let daily_forecasts = [];
+            for (let i = 0; i < 7; i++) {
+                daily_forecasts.push({
+                    date: weather_data.daily[i].dt,
+                    temp: Math.round(weather_data.daily[i].temp.day),
+                    weather_state: weather_data.daily[i].weather[0].main,
+                    weather_icon: weather_data.daily[i].weather[0].icon,
+                    weather_description: weather_data.daily[i].weather[0].description,
+                    wind_speed: Math.round(weather_data.daily[i].wind_speed),
+                    wind_direction: weather_data.daily[i].wind_deg,
+                });
+            }
+            return daily_forecasts;
+        };
         aggregateData = {
             current: {
                 today: weather_data.current.dt,
@@ -35,6 +62,7 @@ app.get('/', (_req, res) => {
                 wind_speed: Math.round(weather_data.current.wind_speed),
                 wind_direction: weather_data.current.wind_deg,
             },
+            daily: daily(weather_data),
         };
         res.send(aggregateData);
     });
--- index.ts
+++ index.ts
@@ -10,17 +10,78 @@ const port = process.env.PORT;
 const lat = process.env.LAT;
 const lon = process.env.LON;
 
+
+//Extract types later
+type DailyData = {
+    date: any;
+    temp: any;
+    weather_state: any;
+    weather_icon: any;
+    weather_description: any;
+    wind_speed: any;
+    wind_direction: any;
+}[];
+
+
+type AggregateData = {
+    current: {
+        today: any;
+        temp: any;
+        feels_like: any;
+        weather_state: any;
+        weather_icon: any;
+        weather_description: any;
+        wind_speed: any;
+        wind_direction: any;
+    };
+    daily: DailyData
+}
+
 app.get('/', (_req: Request, res: Response) => {
 
     let lat = process.env.LAT;
     let lon = process.env.LON;
     let api_key = process.env.OPEN_WEATHER_API_KEY;
     let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}&units=imperial`;
-    let aggregateData = {};
+    let aggregateData: AggregateData = {
+        current: {
+            today: '',
+            temp: '',
+            feels_like: '',
+            weather_state: '',
+            weather_icon: '',
+            weather_description: '',
+            wind_speed: '',
+            wind_direction: '',
+        },
+        daily: [],
+    };
 
-    fetch(url, aggregateData)
+    fetch(url)
         .then(res => res.json())
         .then(weather_data => {
+            let daily = (weather_data: { 
+                daily: {
+                    dt: any;
+                    temp: any;
+                    weather: any;
+                    wind_speed: any;
+                    wind_deg: any;
+                }[]; }) => {
+                let daily_forecasts = [];
+                for (let i = 0; i < 7; i++) {
+                    daily_forecasts.push({
+                        date: weather_data.daily[i].dt,
+                        temp: Math.round(weather_data.daily[i].temp.day),
+                        weather_state: weather_data.daily[i].weather[0].main,
+                        weather_icon: weather_data.daily[i].weather[0].icon,
+                        weather_description: weather_data.daily[i].weather[0].description,
+                        wind_speed: Math.round(weather_data.daily[i].wind_speed),
+                        wind_direction: weather_data.daily[i].wind_deg,
+                    });
+                }
+                return daily_forecasts;
+            } 
             aggregateData = {
                 current: {
                     today: weather_data.current.dt, //UTC Timestamp
@@ -28,15 +89,17 @@ app.get('/', (_req: Request, res: Response) => {
                     feels_like: 
                         //Check if the "Feels Like" temp is signficantly different from actual temp, otherwise return an empty string (falsy).
                         Math.abs(weather_data.current.temp - weather_data.current.feels_like) > 5 
-                        ? weather_data.current.feels_like
-                        : '',
+                            ? weather_data.current.feels_like
+                            : '',
                     weather_state: weather_data.current.weather[0].main,
                     weather_icon: weather_data.current.weather[0].icon,
                     weather_description: weather_data.current.weather[0].description,
                     wind_speed: Math.round(weather_data.current.wind_speed),
                     wind_direction: weather_data.current.wind_deg,
                 },
+                daily: daily(weather_data),
             };
+            
             res.send(aggregateData);
         })
 });
